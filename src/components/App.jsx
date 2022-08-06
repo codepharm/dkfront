22b5d3f6b98f8a43a08e396dedd0dc87491b2bd1
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-08-06 11:48:41 -0400

    init commit w bootstrap router framer

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Services from '../pages/Services';
+import Nutrition from '../pages/Nutrition';
+import Products from '../pages/Products';
+import Gallery from '../pages/Gallery';
+import Contact from '../pages/Contact';
+
+import '../styles/global.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path='/*' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/nutrition' element={<Nutrition />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/gallery' element={<Gallery />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
