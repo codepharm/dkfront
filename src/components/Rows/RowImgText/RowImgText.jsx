a4b84f90f9f8f26381a5d6fc895132115240331e
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-09-08 18:14:07 -0400

    add motion to row components

--- src/components/Rows/RowImgText/RowImgText.jsx
+++ src/components/Rows/RowImgText/RowImgText.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import spanMap from '../../../utils/spanMap';
 import styles from './RowImgText.module.css';
 
@@ -13,17 +14,27 @@ const RowImgText = ({img, alt, heading, text, imgRight}) => {
             { spanMap(text) }
           </p>
         </div>
-        <div className={`col-sm-5`}>
+        <motion.div
+          className={`col-sm-5`}
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5}}
+        >
           <img src={img} className={`p-4 img-fluid`} alt={alt} />
-        </div>
+        </motion.div>
       </div>
     )
   }
   return (
     <div className={`${styles.row} row container-fluid`}>
-      <div className={`col-sm-5`}>
+      <motion.div
+        className={`col-sm-5`}
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5}}
+      >
         <img src={img} className={`p-4 img-fluid`} alt={alt} />
-      </div>
+      </motion.div>
       <div className={`col-sm-7 m-auto py-5 px-4`}>
         <h4 className={`fw-light`}>
           { spanMap(heading) }
