9fccf8453f3a7935d82047062ad6e5f506df4afe
Sidany Walker <sidanyk@gmail.com>
2022-09-11 09:12:47 -0400

    capitalized send message

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -185,7 +185,7 @@ const Form = ({recaptchaRef}) => {
           className={`${styles.button}`}
           onClick={(e) => clickHandler(e)}
         >
-          { disable || !token || token === '' ? 'COMPLETE FORM' : 'Send Message' }
+          { disable || !token || token === '' ? 'COMPLETE FORM' : 'SEND MESSAGE' }
         </button>
       </div>
     </form>
